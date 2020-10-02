b60b346d470b130c6d2721a4ecfc9ccf8d61aef8
Crowdbotics <team@crowdbotics.com>
2020-10-02 01:17:34 +0000

    Initial commit.

---
+++ backend/recevent_20958/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for recevent_20958 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recevent_20958.settings')
+
+application = get_wsgi_application()
