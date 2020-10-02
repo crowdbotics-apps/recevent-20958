29d93d87bcc6247dec2c5a78bfe386f1948af735
Crowdbotics <team@crowdbotics.com>
2020-10-02 01:28:49 +0000

    Update models

--- backend/recevent_20958/wsgi.py
+++ backend/recevent_20958/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recevent_20958.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recevent_20958.settings")
 
 application = get_wsgi_application()
